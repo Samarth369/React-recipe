71721fb1605dcf6e5e6706dafb849f65197de9ab
Samarth369 <samarthpawar6138@gmail.com>
2025-05-14 01:24:58 +0530

    completed about section

--- src/About.jsx
+++ src/About.jsx
@@ -1,3 +1,10 @@
+import Nav from './componentes/Nav'
+
 export default function About () {
-    
+    return (
+        <>
+        <Nav />
+        <h1>Hi!! this is a react project</h1>
+        </>
+    )
 }
